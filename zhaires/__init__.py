dca7b6abb713f430075d8072f3523ec66758c590
Remy Prechelt <rprechelt@gmail.com>
2019-11-28 13:52:36 -1000

    Added submodule import after defining run_directory

--- zhaires/__init__.py
+++ zhaires/__init__.py
@@ -1,5 +1,4 @@
 import os
-from .aires import Task
 
 # the current version
 __version__ = "0.1.0"
@@ -10,3 +9,7 @@ run_directory = os.getenv("AIRES_RUN_DIR")
 # check that the right environment variable is defined
 if not run_directory:
     raise ValueError(f"AIRES_RUN_DIR is not defined. Please defined AIRES_RUN_DIR.")
+
+# these modules may depend on run_directory
+from .aires import Task
+from .loader import load_waveforms
