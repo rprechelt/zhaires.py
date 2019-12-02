4d6789c6fa333bd55736f53b7ebc261b5e9dc2c7
Remy Prechelt <rprechelt@gmail.com>
2019-12-02 11:10:07 -1000

    Add function to check whether simulation exists

--- zhaires/__init__.py
+++ zhaires/__init__.py
@@ -13,3 +13,26 @@ if not run_directory:
 # these modules may depend on run_directory
 from .aires import Task
 from .loader import load_waveforms, load_properties
+
+def exists(simname: str, directory: str = run_directory) -> bool:
+    """
+    Return True if `simname` already exists in `directory`.
+
+    Parameters
+    ----------
+    simname: str
+        The name of the simulation.
+    directory: str
+        The run directory to check.
+
+    Returns
+    -------
+    exists: bool
+        Return True if `simname` already exists.
+    """
+
+    # check if the simulation has already been start
+    exists = os.path.exists(join(directory, *(simname, 'Zhaires.status')))
+
+    # and we are done
+    return exists
