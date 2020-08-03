451a11f4532322d4627d427610517a95bc2f5e4f
Remy Prechelt <rprechelt@gmail.com>
2020-08-03 10:26:35 -1000

    Add support for lateral and ground table loading.

--- zhaires/table.py
+++ zhaires/table.py
@@ -1,12 +1,18 @@
 """
 Generate and load Aires data tables.
 """
-import subprocess
 import os.path as op
+import re
+import subprocess
+
 import numpy as np
+
+from xarray.core.dataarray import DataArray
+
 from .path import get_run_directory
 from .utils import find_aires
-from xarray.core.dataarray import DataArray
+
+__all__ = ["load_table", "generate_table"]
 
 
 def load_table(sim: str, table: int, directory: str = get_run_directory()) -> DataArray:
@@ -41,18 +47,21 @@ def load_table(sim: str, table: int, directory: str = get_run_directory()) -> Da
     if not op.exists(filename):
         raise ValueError(f"Unable to load t{table} for {filename}")
 
+    # read the whole filena
+    table_type = parse_table_type(filename)
+
     # load the raw data
     raw = np.loadtxt(filename)
 
-    # load the data table into an XArray
-    data = DataArray(
-        raw[:, 1:],
-        dims=["level", "quantity"],
-        coords={
-            "level": np.arange(raw.shape[0]),
-            "quantity": ["depth", "mean", "rms", "stdev", "min", "max"],
-        },
-    )
+    # parse the table type
+    if table_type == "long":
+        data = parse_longitudinal(raw)
+    elif table_type == "lateral":
+        data = parse_lateral(raw)
+    elif table_type == "energy":
+        data = parse_energy(raw)
+    else:
+        raise ValueError(f"Unknown table type: {table_type}")
 
     # and set the units for each quantity
     data.attrs["units"] = {
@@ -63,7 +72,7 @@ def load_table(sim: str, table: int, directory: str = get_run_directory()) -> Da
         "energy": "GeV",
     }
 
-    # and we are done
+    # and return the constructed table
     return data
 
 
@@ -105,3 +114,123 @@ def generate_table(
 
     # the table was generated successfully - load it and return it to the user
     return load_table(sim, table, directory)
+
+
+def parse_table_type(filename: str) -> str:
+    """
+    Parse the table type from a file.
+
+    Parameters
+    ----------
+    filename: str
+        The filename of a table.
+
+    Returns
+    -------
+    table_type: str
+        The type of this table.
+    """
+
+    # open the filename
+    with open(filename, "r") as f:
+        contents = f.read()[0:600]  # read the whole file and take 600
+
+        # now match the contents of the line against regex
+        if re.search(r"Longitudinal development: Energy", contents):
+            return "energy"
+        elif re.search(r"Longitudinal development:", contents):
+            return "long"
+        elif re.search(r"Lateral distribution:", contents):
+            return "lateral"
+        elif re.search(r"Unweighted lateral distribution:", contents):
+            return "lateral"
+        elif re.search(r"Energy distribution", contents):
+            return "energy"
+        elif re.search(r"Energy distribution", contents):
+            return "energy"
+        else:
+            raise ValueError(f"Unknown table type.")
+
+
+def parse_lateral(data: np.ndarray) -> DataArray:
+    """
+    Parse raw data into energy table.
+
+    Parameters
+    ----------
+    data: np.ndarray
+        The raw data for a energy table.
+
+    Returns
+    -------
+    table: DataArray
+        The created xarray.DataArray
+    """
+    # load the data table into an XArray
+    table = DataArray(
+        data[:, 1:],
+        dims=["level", "quantity"],
+        coords={
+            "level": np.arange(data.shape[0]),
+            "quantity": ["R", "mean", "rms", "stdev", "min", "max"],
+        },
+    )
+
+    # and we are done
+    return table
+
+
+def parse_energy(data: np.ndarray) -> DataArray:
+    """
+    Parse raw data into energy table.
+
+    Parameters
+    ----------
+    data: np.ndarray
+        The raw data for a energy table.
+
+    Returns
+    -------
+    table: DataArray
+        The created xarray.DataArray
+    """
+    # load the data table into an XArray
+    table = DataArray(
+        data[:, 1:],
+        dims=["level", "quantity"],
+        coords={
+            "level": np.arange(data.shape[0]),
+            "quantity": ["energy", "mean", "rms", "stdev", "min", "max"],
+        },
+    )
+
+    # and we are done
+    return table
+
+
+def parse_longitudinal(data: np.ndarray) -> DataArray:
+    """
+    Parse raw data into a longitudinal table.
+
+    Parameters
+    ----------
+    data: np.ndarray
+        The raw data for a longitudinal table.
+
+    Returns
+    -------
+    table: DataArray
+        The created xarray.DataArray
+    """
+    # load the data table into an XArray
+    table = DataArray(
+        data[:, 1:],
+        dims=["level", "quantity"],
+        coords={
+            "level": np.arange(data.shape[0]),
+            "quantity": ["depth", "mean", "rms", "stdev", "min", "max"],
+        },
+    )
+
+    # and we are done
+    return table
